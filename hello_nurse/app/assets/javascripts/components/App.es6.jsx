c829274878a8180ceee01aa8d329db9ea7906588
John Talio <johntalio@yahoo.com>
2017-01-29 15:43:35 -0500

    Integrate searchbar component into app

--- hello_nurse/app/assets/javascripts/components/App.es6.jsx
+++ hello_nurse/app/assets/javascripts/components/App.es6.jsx
@@ -1,13 +1,24 @@
 class App extends React.Component {
   constructor() {
     super()
-    this.state = {medData: []}
+    this.state = {medCollection: []}
+    this.medData = this.medData.bind(this)
   }
-  
+
+  medData(searchDoctor) {
+    $.ajax({
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${searchDoctor}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=9b916adf7c966d9dce5bb16db9f4b2b2`
+    }).done((response) => {
+      console.log(response);
+      this.setState({medCollection: [response]})
+    })
+  }
+
   render() {
     return(
       <div>
-        <h1>Hello, world!</h1>
+        <h1>Eh, What's Up Doc?</h1>
+        <SearchBar medData={this.medData} />
       </div>
     )
   }
