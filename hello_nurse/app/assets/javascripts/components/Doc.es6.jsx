12f983fd2de762a29d9db2eed8182ee0b6a5d97a
John Talio <johntalio@yahoo.com>
2017-01-29 16:37:45 -0500

    Add component for showing attributes of each doc

---
+++ hello_nurse/app/assets/javascripts/components/Doc.es6.jsx
@@ -0,0 +1,20 @@
+class Doc extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    let { first_name, last_name } = this.props.doctorInfo
+    return(
+      <div className="doctor-info">
+        <li className="doctor-list">
+          <p>
+            <span className="doctor-first-name">First Name: {first_name}</span><br />
+            <span className="doctor-last-name">Last Name: {last_name}</span>
+          </p>
+          <a href="" />
+        </li>
+      </div>
+    )
+  }
+}
