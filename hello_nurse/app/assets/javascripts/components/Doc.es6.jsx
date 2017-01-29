bfb10b2a5dfcaf0c3b63fb54e4e168fb21522b8f
John Talio <johntalio@yahoo.com>
2017-01-29 18:54:32 -0500

    Utilize uid to display individual doctor

--- hello_nurse/app/assets/javascripts/components/Doc.es6.jsx
+++ hello_nurse/app/assets/javascripts/components/Doc.es6.jsx
@@ -5,14 +5,16 @@ class Doc extends React.Component {
 
   render() {
     let { first_name, last_name } = this.props.doctorInfo
+    let { uid } = this.props.doctor
     return(
       <div className="doctor-info">
         <li className="doctor-list">
           <p>
             <span className="doctor-first-name">First Name: {first_name}</span><br />
-            <span className="doctor-last-name">Last Name: {last_name}</span>
+            <span className="doctor-last-name">Last Name: {last_name}</span><br />
+            <span className="doctor-uid">UID: {uid}</span>
           </p>
-          <a href="" />
+          <a href={`/docs/${uid}`}>More Info</a>
         </li>
       </div>
     )
